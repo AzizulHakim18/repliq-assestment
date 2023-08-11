670cc5769fddc3aedc72350ac2a4b475165ae0ed
AzizulHakim18 <hakimrony18@gamil.com>
2023-08-12 00:49:15 +0600

    project set up and navbar section added

---
+++ src/Routes/Router.js
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home";
+import About from "../Pages/About";
+import Contact from "../Pages/Contact";
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+            {
+                path: "/about",
+                element: <About></About>
+            },
+            {
+                path: "/contact",
+                element: <Contact></Contact>
+            },
+        ]
+    }
+])
